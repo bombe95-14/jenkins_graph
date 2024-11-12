d1b34be4bdb3e3b95221305e6a0d0a07365fc54d
bombe95-14 <kenfackkeng1995@gmail.com>
2024-11-12 09:39:22 +0100

    first commit

---
+++ src/main/java/com/example/reactif/vertx/MainVerticle.java
@@ -0,0 +1,41 @@
+package com.example.reactif.vertx;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.ext.web.Router;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+   /*  vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(8081).onComplete(http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    }); */
+
+       final Router router = Router.router(vertx);
+
+    router.get("/hello/") 
+        .handler(req -> req.response()
+            .putHeader("content-type", "text/html") 
+            .end("<html><body><h1>Hello World</h1></body></html>"));
+
+    router.get("/byebye/") 
+        .handler(req -> req.response()
+            .putHeader("content-type", "text/html") 
+            .end("<html><body><h1>Bye Bye World</h1></body></html>"));
+
+            vertx.createHttpServer() 
+            .requestHandler(router::accept) 
+            .listen(8080);        
+
+  }
+}
