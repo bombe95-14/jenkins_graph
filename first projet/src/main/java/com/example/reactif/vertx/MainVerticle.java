0f057f20f81f7d88ea8ef1e524b3f9a4dd53b3c7
bombe95-14 <kenfackkeng1995@gmail.com>
2024-12-24 14:06:15 +0100

    push kenfack du 24/12/2024 V0

--- first projet/src/main/java/com/example/reactif/vertx/MainVerticle.java
+++ first projet/src/main/java/com/example/reactif/vertx/MainVerticle.java
@@ -3,15 +3,22 @@ package com.example.reactif.vertx;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-     vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
+
+   /*   vertx.createHttpServer().requestHandler(req -> {
+   
+      if ( req.path().startsWith("toto") ) {
+          req.response().end("welcome vertx");        
+      }
+
+      //req.response()
+      //  .putHeader("content-type", "text/plain")
+      //  .end("Hello from Vert.x!");
     }).listen(8081).onComplete(http -> {
       if (http.succeeded()) {
         startPromise.complete();
@@ -19,23 +26,42 @@ public class MainVerticle extends AbstractVerticle {
       } else {
         startPromise.fail(http.cause());
       }
-    }); 
-/* 
+    });  */
+ 
+    vertx.deployVerticle(new HelloVerticle());
        final Router router = Router.router(vertx);
 
-    router.get("/hello/") 
-        .handler(req -> req.response()
-            .putHeader("content-type", "text/html") 
-            .end("<html><body><h1>Hello World</h1></body></html>"));
+    router.get("/eleve/list") 
+        .handler( this::getAllStudent );
 
-    router.get("/byebye/") 
-        .handler(req -> req.response()
-            .putHeader("content-type", "text/html") 
-            .end("<html><body><h1>Bye Bye World</h1></body></html>"));
+    router.get("/matiere/:list") 
+        .handler( this::getAllCourseUnit );
+
+
 
             vertx.createHttpServer() 
-         //   .requestHandler(router::accept) 
-            .listen(8080);        */ 
+            .requestHandler(router) 
+            .listen(8080);         
+
+  }
+
+  void getAllStudent( RoutingContext routingContext ){
+
+    vertx.eventBus().request("hello.vertx.addr", "", reply->{
+      routingContext.request().response()
+     // .putHeader("content-type", "text/html") 
+      .end( (String) reply.result().body() );
+    }); 
+
+  }
+
+  void getAllCourseUnit( RoutingContext routingContext ){
+
+    System.out.println("jkoiogiepoh erjhopieroph");
+    String name = routingContext.pathParam("variable");
+    routingContext.request().response()
+      .putHeader("content-type", "text/html") 
+      .end("<html><body><h1>Bye Bye World "+ name +" </h1></body></html>");
 
   }
 }
